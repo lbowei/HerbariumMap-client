2297d886d3dea0aab4660ffcfcb4b8aeb2e385e4
Longbo Wei <lbowei@unc.edu>
2020-10-13 21:21:27 -0400

    123

---
+++ app.js
@@ -0,0 +1,34 @@
+import "https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js";
+const mapbox_token =
+  "pk.eyJ1IjoiaG9uZ3l1bGkiLCJhIjoiY2tnOGhrdGtkMGhlMTJzcGdmeDR1dHpmeCJ9._qlZmG9Xr-XQo6ztXOfz3Q";
+
+mapboxgl.accessToken = mapbox_token;
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 1.5,
+  center: [16, 33],
+});
+
+
+// const getColorFromCount = count =>{
+
+// }
+
+
+
+fetch(" http://localhost:5000/herbarium/")
+  .then((response) => response.json())
+  .then((data) => {
+    const herbariums = data;
+    console.log(data);
+    herbariums.forEach((herbarium) => {
+      const { Herbariumname, latitude, longitude } = herbarium;
+      console.log(latitude);
+      // console.log(herbarium)
+      var marker = new mapboxgl.Marker({ color: "#7BAFD4" })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+    });
+  });
